ca4ad6fe6d7ed825466a50f1cd05e08129f96a5b
gerard-sog <gerard-columbia-sog@outlook.com>
2026-05-27 21:27:21 +0200

    Added all rtbf backpacks

--- mods/backpacks/config.cpp
+++ mods/backpacks/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
         units[] = {};
         weapons[] = {};
         requiredVersion = 0.1;
-        requiredAddons[] = {"A3_Characters_F", "rtbf_rucks"};
+        requiredAddons[] = {"A3_Characters_F", "rtbf_rucks", "sog_rucksacks", "bf_cw_rucks", "characters_f_vietnam_c", "ace_compat_sog"};
     };
 };
 
@@ -20,6 +20,240 @@ class CfgVehicles
     };
 
     class rtbf_mountain_1_od: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class rtbf_tropical_1_od: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class seal_radio_1_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class seal_radio_2_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class seal_radio_3_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class seal_radio_4_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class seal_radio_5_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_1: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_2: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_3: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_bird: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_dude: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_fen: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_franz: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_jimbo: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_mac: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_mike: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_power: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_stevie: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class bf_cw_med_proxno: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_arvn_1_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_arvn_2_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_arvn_3_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_arvn_4_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_ciso_1_a_khaki: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_ciso_1_a_khaki_rto: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_ciso_1_a_khaki_spray: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_ciso_1_a_khaki_spray_rto: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_ciso_1_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_ciso_1_a_rto: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_ciso_1_a_spray: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_ciso_1_a_spray_rto: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_ciso_1_b_khaki: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_ciso_1_b_khaki_spray: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_ciso_1_b: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_ciso_1_b_spray: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_trop_1_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_trop_3_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_trop_4_a: B_Kitbag_cbr
+    {
+        maximumLoad = 100;
+        mass = 30;
+    };
+
+    class sog_trop_2_a: B_Kitbag_cbr
     {
         maximumLoad = 100;
         mass = 30;
