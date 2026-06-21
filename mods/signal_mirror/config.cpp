e16c07079cfac98a7a7da5993063fef198ec0482
gerard-sog <gerard-columbia-sog@outlook.com>
2026-06-21 17:02:48 +0200

    WIP

--- mods/signal_mirror/config.cpp
+++ mods/signal_mirror/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches
         author = "Gerard";
         requiredAddons[] = {"cba_main"};
         units[] = {};
-        weapons[] = {};
+        weapons[] = {"SM_SignalMirror"};
     };
 };
 
@@ -21,6 +21,54 @@ class CfgFunctions
             class init {};
             class startAirScanner {};
             class showSignalMirrorIcon {};
+        };
+    };
+};
+
+class CfgWeapons
+{
+    class Binocular;
+
+    class SM_SignalMirror: Binocular
+    {
+        author = "Gerard";
+        displayName = "Signal Mirror";
+
+        scope = 2;
+        scopeArsenal = 2;
+
+        model = "\signal_mirror\models\signal_mirror.p3d";
+        picture = "\signal_mirror\assets\ui\signal_mirror_ca.paa";
+
+        modelOptics = "";
+        simulation = "Binocular";
+        weaponPoolAvailable = 1;
+
+        class OpticsModes
+        {
+            class Binocular
+            {
+                opticsID = 1;
+                useModelOptics = 0;
+
+                opticsZoomMin = 0.25;
+                opticsZoomMax = 0.25;
+                opticsZoomInit = 0.25;
+
+                discretefov[] = {0.25};
+                discreteInitIndex = 0;
+
+                distanceZoomMin = 100;
+                distanceZoomMax = 100;
+
+                memoryPointCamera = "eye";
+                cameraDir = "";
+
+                visionMode[] = {"Normal"};
+                opticsFlare = 0;
+                opticsDisablePeripherialVision = 0;
+                opticsPPEffects[] = {};
+            };
         };
     };
 };
